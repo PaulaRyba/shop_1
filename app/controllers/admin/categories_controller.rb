a7624cbe93efc3ed2af1dcdca79756d98929de81
rybka <rybka@akwarium>
2013-11-21 01:04:12 +0100

    admin categories update fix

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -65,6 +65,6 @@ class Admin::CategoriesController < AdminController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def category_params
-      params.require(:category).permit(:name)
+      params.require(:category).permit(:title)
     end
 end
